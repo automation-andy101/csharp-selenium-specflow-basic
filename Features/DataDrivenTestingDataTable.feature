151ae3e9377eeaffea1bb6b61810ef7ac8900948
Andy Short <automation.andy101@gmail.com>
2023-03-08 06:49:15 +0000

    Added data driven tests

---
+++ Features/DataDrivenTestingDataTable.feature
@@ -0,0 +1,14 @@
+﻿Feature: Data Table Data Driven Testing Examples
+
+A short summary of the feature
+
+@TestersTalk
+Scenario: Search for Testers Talk using data driven scenario outline
+	Given I am on the youtube search page
+	When Enter search keyword in YouTube
+	| searchKey				   |
+	| specflow by testers talk |
+	| selenium by testers talk |
+
+
+
---
+++ StepDefinitions/DataTableDataDrivenTestingExamplesStepDefinitions.cs
@@ -0,0 +1,37 @@
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecFlowEA.StepDefinitions
+{
+    [Binding]
+    public class DataTableDataDrivenTestingExamplesStepDefinitions
+    {
+        private IWebDriver driver;
+
+        public DataTableDataDrivenTestingExamplesStepDefinitions(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        [When(@"Enter search keyword in YouTube")]
+        public void WhenEnterSearchKeywordInYouTube(Table table)
+        {
+            var searchCriteria = table.CreateSet<SearchKeyTestData>();
+
+            foreach (var keyword in searchCriteria)
+            {
+                driver.FindElement(By.XPath("//*[@name='search_query']")).Clear();
+                driver.FindElement(By.XPath("//*[@name='search_query']")).SendKeys(keyword.searchKey);
+            }
+        }
+
+
+    }
+
+    public class SearchKeyTestData
+    {
+        public string searchKey{ get; set; }
+    }
+}
